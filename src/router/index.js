ccf252b7e8f60906dee0bd130b226ee1436de3fe
Mango-pomelo-sago <806697001@qq.com>
2026-08-21 23:26:40 +0800

    fix: GitHub Pages 使用 hash 路由，深链直达 #/admin 不再 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainLayout from '../layouts/MainLayout.vue'
 
 const routes = [
@@ -80,7 +80,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
